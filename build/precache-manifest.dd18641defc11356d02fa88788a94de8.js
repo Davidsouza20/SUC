cf58d35ba852d393e9193ae0c94b1139293dc9da
David Souza <david_jsouza@hotmail.com>
2020-02-18 22:19:23 -0300

    package error

---
+++ build/precache-manifest.dd18641defc11356d02fa88788a94de8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "12dfea748eda6638fa3475a210a0876d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "40811a335b2cea1c03ee",
+    "url": "/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "cc8be8a0cf8cda76d34e",
+    "url": "/static/css/main.15394d2d.chunk.css"
+  },
+  {
+    "revision": "40811a335b2cea1c03ee",
+    "url": "/static/js/2.cf84e171.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/static/js/2.cf84e171.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cc8be8a0cf8cda76d34e",
+    "url": "/static/js/main.4cae1d58.chunk.js"
+  },
+  {
+    "revision": "a8139511e7e3c6f8467f",
+    "url": "/static/js/runtime-main.1c2d59f4.js"
+  },
+  {
+    "revision": "d7d5d4588a9f50c99264bc12e4892a7c",
+    "url": "/static/media/OpenSans-Regular.d7d5d458.ttf"
+  }
+]);
